8f2eed75f0da203272bd8d04c68b712198cc2d3b
dev9 <dev9@dev9s-mac-mini.local>
2016-11-22 16:54:31 +0545

    Initial Commit

---
+++ aryalN-NewsPortalTests/aryalN_NewsPortalTests.swift
@@ -0,0 +1,36 @@
+//
+//  aryalN_NewsPortalTests.swift
+//  aryalN-NewsPortalTests
+//
+//  Created by dev9 on 11/22/16.
+//  Copyright © 2016 ebpearls. All rights reserved.
+//
+
+import XCTest
+@testable import aryalN_NewsPortal
+
+class aryalN_NewsPortalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
